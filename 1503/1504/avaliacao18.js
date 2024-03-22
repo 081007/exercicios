0f83f8f6641a1df8dd6ad27aa33057582e7a6e67
anna luiza <annaluizaoliveiragomes418@gmail.com>
2024-03-22 15:48:44 -0300

    novo commit

--- 1503/1504/avaliacao18.js
+++ 1503/1504/avaliacao18.js
@@ -1,13 +1,14 @@
 let prompt = require('prompt-sync')();
 
-let n1;
-let n2;
-let n3;
-
-
 n1 = parseInt(prompt('digite a primeira nota: '));
 n2 = parseInt(prompt('digite a segunda nota: '));
+while (n2 = n1){
+    n2 = parseInt(prompt('Digite um valor diferente de n1: '))
+}
 n3 = parseInt(prompt('digite a terceira nota: '));
+while(n3 = n2 & n3 === n1){
+    n3 = parseInt(prompt('digite um valor diferente: '))
+}
 
 if (n1 > n2 & n1 > n3){
     console.log('O maior é: ', n1)
@@ -18,8 +19,3 @@ else if (n2 > n1 & n2 > n3){
 else {
     console.log('O maior é: ', n3)
 }
-if ((n1 = n2 = n3))
-    console.log('Negado!');{
-    
- }
- 
