21d0507c2b7aa1d99b94a6dab0a7420355f05c33
anna luiza <annaluizaoliveiragomes418@gmail.com>
2024-03-22 16:53:17 -0300

    exercicios resolvidos

--- 1503/1504/selecao.js/prova5.js
+++ 1503/1504/selecao.js/prova5.js
@@ -3,7 +3,7 @@ n1 = parseInt(prompt('digite a nota1: '));
 n2 = parseInt(prompt('digite a nota2: '));
 media =  (n1+n2)/2;
 console.log('A média do(a) aluno(a) é; ', );
-if  ((n1 >= 0) & ( n1<= 10) & (n2 >= 0) & (n2 <=10)){
+if  ((n1 >= 0)  & ( n1<= 10) & (n2 >= 0) & (n2 <=10)){
     console.log('Nota válida!')}
 else  {
     console.log('Nota inválida!') 
